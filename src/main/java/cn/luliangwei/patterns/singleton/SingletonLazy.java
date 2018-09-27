fdd593abf30d6eedb787220f8f373a6b7d215e49
luliangwei <luliangwei1874@163.com>
2018-09-27 21:31:59 +0800

    1、update

--- src/main/java/cn/luliangwei/patterns/singleton/SingletonLazy.java
+++ src/main/java/cn/luliangwei/patterns/singleton/SingletonLazy.java
@@ -5,6 +5,8 @@ package cn.luliangwei.patterns.singleton;
  * 
  * 缺陷：效率不高，因为在任何时候只能有一个线程调用 getInstance() 方法。
  * 
+ * 这里的synchronized相当于一个全局锁
+ * 
  * 参考博客：http://wuchong.me/blog/2014/08/28/how-to-correctly-write-singleton-pattern/
  */
 public class SingletonLazy {
